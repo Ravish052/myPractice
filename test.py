22969ef58f5f53ab327515ffaa1ba455e01bcfb7
ravish463 <125432283+ravish463@users.noreply.github.com>
2024-09-05 21:47:58 +0530

    git INIT

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import subprocess
+import time
 
+
+unix_timestamp = int(time.time())
+
+# Your string
+original_string = "This is my string"
+
+# Append the Unix timestamp to the string
+result_string = f"{original_string} {unix_timestamp}"
+
+# Print the result
+print(result_string)
 # Run git add .
 subprocess.run(["git", "add", "."], check=True)
 
@@ -9,4 +21,4 @@ subprocess.run(["git", "commit", "-m", "git INIT"], check=True)
 # Run git push
 subprocess.run(["git", "push"], check=True)
 
-print("hel")
+
